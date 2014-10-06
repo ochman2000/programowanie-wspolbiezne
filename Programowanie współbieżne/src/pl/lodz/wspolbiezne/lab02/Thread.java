8ec090024d44ac468f3b43dfc2dbaf96fb24c54e
Łukasz Ochmański <ochman2000@gmail.com>
2014-10-06 10:10:56 +0200

    Zdefiniowana klasa Thread

--- Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab02/Thread.java
+++ Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab02/Thread.java
@@ -2,10 +2,47 @@ package pl.lodz.wspolbiezne.lab02;
 
 public class Thread implements Runnable {
 
+	private int startIndex;
+	private int endIndex;
+	private byte[] tablicaBajtow;
+	private byte[] histogram;
+	
 	@Override
 	public void run() {
-		
-		
+		for (int i = startIndex; i < endIndex; i++) {
+			histogram[tablicaBajtow[i]]++;
+		}
+	}
+	
+	public int getStartIndex() {
+		return startIndex;
 	}
 
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+
+	public int getEndIndex() {
+		return endIndex;
+	}
+
+	public void setEndIndex(int endIndex) {
+		this.endIndex = endIndex;
+	}
+
+	public byte[] getTablicaBajtow() {
+		return tablicaBajtow;
+	}
+
+	public void setTablicaBajtow(byte[] tablicaBajtow) {
+		this.tablicaBajtow = tablicaBajtow;
+	}
+
+	public byte[] getHistogram() {
+		return histogram;
+	}
+
+	public void setHistogram(byte[] histogram) {
+		this.histogram = histogram;
+	}
 }
