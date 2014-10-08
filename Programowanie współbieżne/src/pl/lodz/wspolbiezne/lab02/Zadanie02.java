c6c55f3db3b3cdfdae1fa427a2f2fa427c33ce76
Łukasz Ochmański <ochman2000@gmail.com>
2014-10-08 23:37:22 +0200

    zapisywanie do pliku

--- Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab02/Zadanie02.java
+++ Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab02/Zadanie02.java
@@ -1,28 +1,32 @@
 package pl.lodz.wspolbiezne.lab02;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Zadanie02 {
-	
-	private static int ROZMIAR_TABLICY = 4_000_000; 
+
+	private static int ROZMIAR_TABLICY = 4_000_000;
 	private W¹tek[] threads;
 	private byte[] tablicaBajtow;
 	private int[] histogram;
 
 	public static void main(String[] args) {
-//		new Zadanie02(1);
-//		new Zadanie02(2);
-//		new Zadanie02(4);
-//		new Zadanie02(6);
-//		new Zadanie02(8);
-//		new Zadanie02(10);
-//		new Zadanie02(12);
-//		new Zadanie02(14);
-		new Zadanie02(16);
+		new Zadanie02(1);
+		// new Zadanie02(2);
+		// new Zadanie02(4);
+		// new Zadanie02(6);
+		// new Zadanie02(8);
+		// new Zadanie02(10);
+		// new Zadanie02(12);
+		// new Zadanie02(14);
+		// new Zadanie02(160);
 	}
-	
+
 	public Zadanie02(int threadsNumber) {
 		long startTime = System.currentTimeMillis();
 		histogram = new int[256];
@@ -30,18 +34,18 @@ public class Zadanie02 {
 		threads = initThreads(threadsNumber);
 		startAll();
 		System.out.println(Arrays.toString(histogram));
-		System.out.println("Liczba bajtów: "+IntStream.of(histogram).sum());
+		System.out.println("Liczba bajtów: " + IntStream.of(histogram).sum());
 		long endTime = System.currentTimeMillis();
-		System.out.println("Czas wykonania: "+(endTime-startTime)+"ms");
+		System.out.println("Czas wykonania: " + (endTime - startTime) + "ms");
 	}
-	
 
 	private W¹tek[] initThreads(int threadsNumber) {
-		if (threadsNumber<0) {
-			throw new IllegalArgumentException("Liczb¹ w¹tków musi byæ wiêksza od 0");
+		if (threadsNumber < 0) {
+			throw new IllegalArgumentException(
+					"Liczb¹ w¹tków musi byæ wiêksza od 0");
 		}
 		W¹tek[] thread = new W¹tek[threadsNumber];
-		for (int interval=0; interval<threadsNumber; interval++) {
+		for (int interval = 0; interval < threadsNumber; interval++) {
 			W¹tek w = new W¹tek();
 			w.setStartIndex(getBeginningOfInterval(interval, threadsNumber));
 			w.setEndIndex(getEndOfInterval(interval, threadsNumber));
@@ -51,34 +55,47 @@ public class Zadanie02 {
 		}
 		return thread;
 	}
-	
+
 	private byte[] initTablicaBajtow() {
-		byte[] tablicaBajtow = new byte[ROZMIAR_TABLICY];
-		new Random().nextBytes(tablicaBajtow);
-		return tablicaBajtow;
+		Path path = Paths.get("resources/bajty.dat");
+		byte[] bajty = null;
+		try {
+			if (!Files.exists(path)) {
+				bajty = new byte[ROZMIAR_TABLICY];
+				new Random().nextBytes(bajty);
+				Files.write(path, bajty);
+			} else {
+				bajty = Files.readAllBytes(path);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return bajty;
 	}
 
 	public static int getBeginningOfInterval(int interval, int totalIntervals) {
-		if (totalIntervals<=interval) {
-			throw new IllegalArgumentException("Przedzia³ nie mo¿e byæ wiêkszy ni¿: "
-					+totalIntervals+" a podano: "+interval);
+		if (totalIntervals <= interval) {
+			throw new IllegalArgumentException(
+					"Przedzia³ nie mo¿e byæ wiêkszy ni¿: " + totalIntervals
+							+ " a podano: " + interval);
 		}
-		double fraction = (double)interval/(double)totalIntervals;
+		double fraction = (double) interval / (double) totalIntervals;
 		return (int) (fraction * ROZMIAR_TABLICY);
 	}
-	
+
 	public static int getEndOfInterval(int interval, int totalIntervals) {
-		double rozmiarPrzedzialu = (double)ROZMIAR_TABLICY/(double)totalIntervals;
-		double fraction = (double)interval/(double)totalIntervals; 
+		double rozmiarPrzedzialu = (double) ROZMIAR_TABLICY
+				/ (double) totalIntervals;
+		double fraction = (double) interval / (double) totalIntervals;
 		return (int) ((fraction * ROZMIAR_TABLICY) + rozmiarPrzedzialu);
 	}
-	
+
 	public void startAll() {
 		int i = 0;
 		for (W¹tek w : threads) {
 			Thread t = new Thread(w);
 			t.start();
-			System.out.println("W¹tek "+ ++i+" uruchmiony");
+			System.out.println("W¹tek " + ++i + " uruchmiony");
 		}
 	}
 }
