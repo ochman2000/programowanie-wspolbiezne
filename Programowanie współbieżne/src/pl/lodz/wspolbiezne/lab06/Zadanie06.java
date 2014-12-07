2961d939ecf4fd3676c8a7a44863cbedfae45237
Lukasz Ochmanski <ochman2000@gmail.com>
2014-12-08 00:31:37 +0100

    zaimplementowanie serwera i klienta

--- Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab06/Zadanie06.java
+++ Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab06/Zadanie06.java
@@ -1,5 +1,11 @@
 package pl.lodz.wspolbiezne.lab06;
 
 public class Zadanie06 {
-	
+	public static void main(String[] args) {
+
+		String hostName = "localhost";
+		int portNumber = 8080;
+		new KnockKnockServer(portNumber);
+		new KnockKnockClient(hostName, portNumber);
+	}
 }
