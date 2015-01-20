bf93a11997f5c973c4616aff917ab66a2f18bf6f
Lukasz Ochmanski <ochman2000@gmail.com>
2015-01-21 00:27:31 +0100

    dane od Michała Drożdża

---
+++ Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab08/Jordan.java
@@ -0,0 +1,31 @@
+package pl.lodz.wspolbiezne.lab08;
+
+public class Jordan implements Runnable {
+	private int beginIndex;
+	private int endIndex;
+	private int currentRow;
+	private int columns;
+	private double[][] matrix;
+
+	public Jordan(int beginIndex, int endIndex, double[][] matrix,
+			int currentRow, int columns) {
+		this.beginIndex = beginIndex;
+		this.endIndex = endIndex;
+		this.matrix = matrix;
+		this.currentRow = currentRow;
+		this.columns = columns;
+	}
+
+	@Override
+	public void run() {
+		for (int r = beginIndex; r < endIndex; r++) {
+			if (r == currentRow) {
+				continue;
+			}
+			double w = matrix[r][currentRow];
+			for (int c = 0; c < columns + 1; c++) {
+				matrix[r][c] -= matrix[currentRow][c] * w;
+			}
+		}
+	}
+}
