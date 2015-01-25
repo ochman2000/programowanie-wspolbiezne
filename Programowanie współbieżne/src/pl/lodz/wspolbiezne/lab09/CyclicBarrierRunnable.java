e54ad73b3474ea25cb5e179ea3bff889a06346a5
Lukasz Ochmanski <ochman2000@gmail.com>
2015-01-25 06:59:14 +0100

    wersja do oddania

--- Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab09/CyclicBarrierRunnable.java
+++ Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab09/CyclicBarrierRunnable.java
@@ -2,35 +2,113 @@ package pl.lodz.wspolbiezne.lab09;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.logging.Logger;
 
 public class CyclicBarrierRunnable implements Runnable {
 
+	double[][] A;
+	private static final boolean BOOLEAN = false;
 	CyclicBarrier barrier1 = null;
 	CyclicBarrier barrier2 = null;
 
-	public CyclicBarrierRunnable(CyclicBarrier barrier1, CyclicBarrier barrier2) {
+	public CyclicBarrierRunnable(CyclicBarrier barrier1, CyclicBarrier barrier2, double[][] A) {
 		this.barrier1 = barrier1;
 		this.barrier2 = barrier2;
+		this.A = A;
 	}
 
 	public void run() {
 		try {
-			System.out.println("W¹tek "+Thread.currentThread().getName()
-					+ " czeka na policzenie partialPivoting/swap przy barierze 1");
-			this.barrier1.await();
+			int n, m, i, j, k;
+			i = 1; j = 1; n = A.length-1; m = A[0].length-1;
+			long starttime = System.currentTimeMillis();
+			System.out.println("n="+n+" m="+m);
+			
+			while (i <= n && j <= m) {
 
-			Thread.sleep(1000);
-			System.out.println("W¹tek "+Thread.currentThread().getName()
-					+ " Eliminate/divide przy barierze 2");
-			this.barrier2.await();
+				k = i;
+				while (k <= n && A[k][j] == 0)
+					k++;
 
-//			Thread.sleep(5000);
-//			System.out.println(Thread.currentThread().getName() + " done!");
+				if (k <= n) {
+					if (k != i) {
+						swap(A, i, k, j);
+					}
+					System.out.println("W¹tek "+Thread.currentThread().getName()
+							+ " czeka na policzenie partialPivoting/swap przy barierze 1");
+					this.barrier1.await();
+					
+					if (A[i][j] != 1) {
+						divide(A, i, j);
+					}
+					eliminate(A, i, j);
+
+					System.out.println("W¹tek "+Thread.currentThread().getName()
+							+ " Eliminate/divide przy barierze 2");
+					this.barrier2.await();
+					
+					i++;
+				}
+				Logger.getGlobal().info("Iteracja nr \t"+j);
+				j++;
+			}
+			long endtime = System.currentTimeMillis();
+			Logger.getAnonymousLogger().info("Czas wykonania: "
+					+(double)((endtime-starttime)/1000.0)+" sekundy.");
+			if (BOOLEAN) { printMatrix(A); }
+			
+			Thread.sleep(5000);
+			System.out.println(Thread.currentThread().getName() + " done!");
 
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (BrokenBarrierException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	private static void swap(double[][] A, int i, int k, int j) {
+		int m = A[0].length - 1;
+		double temp;
+		for (int q = j; q <= m; q++) {
+			temp = A[i][q];
+			A[i][q] = A[k][q];
+			A[k][q] = temp;
+		}
+	}
+	
+	private static void divide(double[][] A, int i, int j) {
+		int m = A[0].length - 1;
+		for (int q = j + 1; q <= m; q++)
+			A[i][q] /= A[i][j];
+		A[i][j] = 1;
+	}
+
+	private static void eliminate(double[][] A, int i, int j) {
+		int n = A.length - 1;
+		int m = A[0].length - 1;
+		for (int p = 1; p <= n; p++) {
+			if (p != i && A[p][j] != 0) {
+				for (int q = j + 1; q <= m; q++) {
+					A[p][q] -= A[p][j] * A[i][q];
+				}
+				A[p][j] = 0;
+			}
+		}
+	}
+
+	private static String printMatrix(double[][] A) {
+		StringBuilder sb = new StringBuilder();
+		int n = A.length - 1;
+		int m = A[0].length - 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++)
+				sb.append(A[i][j] + "  ");
+			sb.append("\n");
+		}
+		sb.append("\n\n");
+		System.out.println(sb);
+		return sb.toString();
 	}
 }
--- Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab09/Zadanie09.java
+++ Programowanie współbieżne/src/pl/lodz/wspolbiezne/lab09/Zadanie09.java
@@ -11,12 +11,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
 
 public class Zadanie09 {
 	private static final String SCIEZKA_DO_PLIKU = "resources/macierze/doubles.dat";
-	private static boolean USE_FILE = false;
-	private static int ROZMIAR_MACIERZY = 1024;
-	private static int LICZBA_W¥TKÓW = 4;
+	private static final boolean USE_FILE = false;
+	private static final int ROZMIAR_MACIERZY = 1024;
+	private static final int LICZBA_W¥TKÓW = 4;
 	
 	public static void main(String[] args) {
 		new Zadanie09();
@@ -24,7 +25,7 @@ public class Zadanie09 {
 	
 	public Zadanie09() {
 		double[][] macierz = initMacierz();
-		new GaussJordan(macierz);
+		performGaussJordan(macierz);
 	}
 	
 	private double[][] initMacierz() {
@@ -56,7 +57,6 @@ public class Zadanie09 {
 	private double[][] getRandomValues() {
 		int n = ROZMIAR_MACIERZY;
 		int m = n+1;
-		// declare d to be of size (n+1)x(m+1) and do not use index 0
 		double[][] d = new double[n+1][m+1];
 		Random rnd = new Random();
 		for (int i=1; i<=n; i++) {
@@ -66,4 +66,53 @@ public class Zadanie09 {
 		}
 		return d;
 	}
+	
+	private void performGaussJordan(double[][] macierz) {
+		Runnable barrier1Action = new Runnable() {
+		    public void run() {
+		    	try {
+					System.out.println("PartialPivoting/Swap trwa...");
+					Thread.sleep(5000);
+			        System.out.println("PartialPivoting/Swap zrobione ");
+			        Thread.sleep(1000);
+			        System.out.println("FORK: Rozdzielenie zadañ na wiele w¹tków");	
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		    }
+		};
+		
+		Runnable barrier2Action = new Runnable() {
+
+			public void run() {
+		    	try {
+					Thread.sleep(5000);
+			    	System.out.println("JOIN: Z³¹czenie wyników obliczeñ.");
+			        Thread.sleep(1000);
+			    	System.out.println("Eliminate/divide dla wszystkich w¹tków zrobione ");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		    }
+		};
+		
+		initializeCyclicBarriers(barrier1Action, barrier2Action, macierz);
+	}
+
+	private void initializeCyclicBarriers(Runnable barrier1Action,
+			Runnable barrier2Action, double[][] macierz) {
+		CyclicBarrier barrier1 = new CyclicBarrier(LICZBA_W¥TKÓW, barrier1Action);
+		CyclicBarrier barrier2 = new CyclicBarrier(LICZBA_W¥TKÓW, barrier2Action);
+		
+		Thread[] threads = new Thread[LICZBA_W¥TKÓW];
+	
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new CyclicBarrierRunnable(barrier1, barrier2,
+					macierz));
+		}
+		for (Thread thread : threads) {
+			thread.start();
+		}
+	}
+	
 }
